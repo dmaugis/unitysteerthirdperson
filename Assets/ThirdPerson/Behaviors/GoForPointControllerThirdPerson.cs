4b42e354a5063a8583ec4e66779e2130261a773d
David MAUGIS <dmaugis@hotmail.com>
2017-12-28 21:35:53 +0100

    first commit

---
+++ Assets/ThirdPerson/Behaviors/GoForPointControllerThirdPerson.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnitySteer.Behaviors;
+
+[RequireComponent(typeof(BipedThirdPerson)), RequireComponent(typeof(SteerForPoint))]
+public class GoForPointControllerThirdPerson : MonoBehaviour 
+{
+	SteerForPoint _steering;
+
+	[SerializeField]
+	Vector3 _pointRange = Vector3.one * 20;
+
+	void Start() 
+	{
+		_steering = GetComponent<SteerForPoint>();
+		_steering.OnArrival += (_) => FindNewTarget();
+		FindNewTarget();
+	}
+
+
+	void FindNewTarget()
+	{
+		_steering.TargetPoint = Vector3.Scale(Random.onUnitSphere, _pointRange);
+		_steering.enabled = true;
+	}
+}
